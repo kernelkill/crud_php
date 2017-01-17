8509969821e8dbbdc94af3ab6abae07b0a4ae4b7
Joabe Guimaraes Querino Kachorroski <Joabe.Kachorroski@wsci.com.br>
2017-01-17 13:56:01 -0300

    Reescrevendo o projeto

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
-<!doctype html>
-<html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Menu</title>
-</head>
-<body>
-<blockquote>
-    <a href="">+ Visualizar Cliente + </a></br>
-    <a href="entidade/add_cliente.php">+ Adicionar Cliente + </a></br>
-</blockquote>
+<?php require_once 'config.php'; ?>
+<?php require_once DBAPI; ?>
 
-</body>
-</html>
+<?php 
+	$db = open_database(); 
+	
+	if ($db) {
+		echo '<h1>Banco de Dados Conectado!</h1>';
+	} else {
+		echo '<h1>ERRO: Não foi possível Conectar!</h1>';
+	}
+?>
--- persistencia/jdbc/ConexaoFactory.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: kachorroski
- * Date: 16/01/17
- * Time: 22:43
- */
