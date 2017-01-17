a4ca9e69eadfb1fa15150363084f07f58f993097
Joabe Guimaraes Querino Kachorroski <Joabe.Kachorroski@wsci.com.br>
2017-01-17 15:28:07 -0300

    Criado o arquivo index e functions, e tbm adicionando uma pasta nova na estrutura do projeto onde vai todas as funçoes de model.

--- config.php
+++ config.php
@@ -1,12 +1,16 @@
 <?php
 /** O nome do banco de dados*/
 define('DB_NAME', 'wda');
+
 /** Usuário do banco de dados MySQL */
 define('DB_USER', 'root');
+
 /** Senha do banco de dados MySQL */
 define('DB_PASSWORD', 'root');
+
 /** nome do host do MySQL */
 define('DB_HOST', 'localhost');
+
 /** caminho absoluto para a pasta do sistema **/
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
@@ -18,3 +22,7 @@ if ( !defined('BASEURL') )
 /** caminho do arquivo de banco de dados **/
 if ( !defined('DBAPI') )
 	define('DBAPI', ABSPATH . 'inc/database.php');
+
+/** caminhos dos templates de header e footer **/
+define('HEADER_TEMPLATE', ABSPATH . 'inc/header.php');
+define('FOOTER_TEMPLATE', ABSPATH . 'inc/footer.php');
